22128efba72036f3c69694e33da0c6bceba63743
Artur <artur.h.0427@gmail.com>
2023-02-25 12:45:56 +0100

    Add getter and setter methods for `Board`

--- src/board.rs
+++ src/board.rs
@@ -1,9 +1,13 @@
 /* Imports */
 
+
+/* Types */
+type Brick = u16;
+
 /* Main */
 struct Board<const N: usize> {
     /// Two-dim array of pieces
-    pieces: [[u16; N]; N],
+    pieces: [[Brick; N]; N],
 }
 
 /* Method impls */
@@ -11,5 +15,16 @@ impl<const N: usize> Board<N> {
     /// Constructor
     pub fn new() -> Self {
         Self { pieces: [[0; N]; N] }
+    }
+
+    /// Getter
+    pub fn get(&self, x: usize, y: usize) -> Option<&Brick> {
+        self.pieces.get(y)?.get(x)
+    }
+    
+    /// Setter
+    pub fn set(&mut self, x: usize, y: usize, to: Brick) -> Option<()> {
+        *self.pieces.get_mut(y)?.get_mut(x)? = to;
+        Some(())
     }
 }
