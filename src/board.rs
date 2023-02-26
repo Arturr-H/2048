5a841d3f642ffd10a83b4f3b2b1fd699f903d282
Artur <artur.h.0427@gmail.com>
2023-02-26 15:13:42 +0100

    Ready `Board` for wasm bindning

--- src/board.rs
+++ src/board.rs
@@ -1,34 +1,39 @@
-use std::fmt::Debug;
-
 /* Imports */
-use rand::{self, Rng, rngs::ThreadRng};
+use std::fmt::Debug;
+use js_sys::Math;
+use wasm_bindgen::prelude::*;
 
 /* Types */
 type Brick = u16;
 
-/* Main */
-pub struct Board<const N: usize> {
-    /// Two-dim array of pieces
-    pieces: [[Brick; N]; N],
+/* Constants */
+const SIZE: usize = 4usize;
 
-    /// The random number generator tied to the game
-    rng: ThreadRng
+/* Main */
+#[wasm_bindgen]
+pub struct Board {
+    /// Two-dim array of pieces
+    pieces: Vec<Vec<Brick>>,
 }
 
 /* Direction for movement */
+#[wasm_bindgen]
+#[repr(u8)]
 pub enum Direction {
     Up, Right, Down, Left
 }
 
 /* Method impls */
-impl<const N: usize> Board<N> {
+#[wasm_bindgen]
+impl Board {
     /// Constructor
+    #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
-        Self { pieces: [[0; N]; N], rng: rand::thread_rng() }
+        Self { pieces: vec![vec![0; SIZE]; SIZE] }
     }
 
     /// Set one random piece
-    pub fn set_random(&mut self) -> () {
+    pub fn set_random(&mut self) {
         let mut empty_tiles = Vec::new();
 
         for (y, row) in self.pieces.iter().enumerate() {
@@ -40,57 +45,63 @@ impl<const N: usize> Board<N> {
         };
 
         if empty_tiles.is_empty() { return; };
-        let coord = empty_tiles[self.rng.gen_range(0..empty_tiles.len())];
-        let value = if self.rng.gen_bool(0.75) { 2u16 } else { 4u16 };
+        let index = Math::random() * empty_tiles.len() as f64;
+        let coord = empty_tiles[index as usize];
+        let value = if Math::random() > 0.25 { 2u16 } else { 4u16 };
         self.set(coord.0, coord.1, value);
     }
 
     /// Getter
-    pub fn get(&self, x: usize, y: usize) -> Option<&Brick> {
-        self.pieces.get(y)?.get(x)
+    fn get(&self, x: usize, y: usize) -> Option<Brick> {
+        self.pieces.get(y)?.get(x).and_then(|e| Some(*e))
     }
     
     /// Setter
-    pub fn set(&mut self, x: usize, y: usize, to: Brick) -> Option<()> {
-        *self.pieces.get_mut(y)?.get_mut(x)? = to;
-        Some(())
+    pub fn set(&mut self, x: usize, y: usize, to: Brick) {
+        match match self.pieces.get_mut(y) {
+            Some(e) => e,
+            None => return
+        }.get_mut(x) {
+            Some(e) => *e = to,
+            None => return
+        };
     }
 
     /// Merge all pieces in direction
-    pub fn merge_all(&mut self, direction: Direction) -> () {
+    pub fn merge_all(&mut self, direction: Direction) {
         match direction {
             Direction::Down => {
                 /* Begin from bottom to up */
-                for y in (0..N).rev() {
-                    for x in 0..N {
+                for y in (0..SIZE).rev() {
+                    for x in 0..SIZE {
                         self.merge_vertical(x, y, 1);
                     }
                 }
             },
             Direction::Up => {
                 /* Begin from top to bottom */
-                for y in 0..N {
-                    for x in 0..N {
+                for y in 0..SIZE {
+                    for x in 0..SIZE {
                         self.merge_vertical(x, y, -1);
                     }
                 }
             },
             Direction::Left => {
                 /* Begin from top to bottom */
-                for y in 0..N {
+                for y in 0..SIZE {
 
                     /* Left to right */
-                    for x in 0..N {
+                    for x in 0..SIZE {
                         self.merge_horizontal(x, y, -1);
                     }
                 }
             },
             Direction::Right => {
                 /* Begin from top to bottom */
-                for y in 0..N {
+                for y in 0..SIZE {
 
                     /* Right to left */
-                    for x in (0..N).rev() {
+                    for x in (0..SIZE).rev() {
                         self.merge_horizontal(x, y, 1);
                     }
                 }
@@ -103,14 +114,14 @@ impl<const N: usize> Board<N> {
     /// `direction`: -1 = up, 1 = down
     fn merge_vertical(&mut self, x: usize, y: usize, direction: isize) -> Option<()> {
         let original_piece = self.get(x, y).unwrap();
-        if original_piece == &0 { return None; };
+        if original_piece == 0 { return None; };
         let mut coords = (x, y);
 
         'main: while let Some(piece) = self.get(coords.0, match coords.1.checked_add_signed(direction) {
             Some(e) => e,
             None => break 'main
         }) {
-            if piece == &0 {
+            if piece == 0 {
                 coords.1 = coords.1.checked_add_signed(direction)?;
             }else if piece == original_piece {
                 self.set(coords.0, coords.1.checked_add_signed(direction)?, piece*2);
@@ -121,7 +132,7 @@ impl<const N: usize> Board<N> {
             }
         }
     
-        self.set(coords.0, coords.1, *original_piece);
+        self.set(coords.0, coords.1, original_piece);
         if coords.1 != y { self.set(x, y, 0); };
         Some(())
     }
@@ -131,14 +142,14 @@ impl<const N: usize> Board<N> {
     /// `direction`: -1 = left, 1 = right
     fn merge_horizontal(&mut self, x: usize, y: usize, direction: isize) -> Option<()> {
         let original_piece = self.get(x, y).unwrap();
-        if original_piece == &0 { return None; };
+        if original_piece == 0 { return None; };
         let mut coords = (x, y);
 
         'main: while let Some(piece) = self.get(match coords.0.checked_add_signed(direction) {
             Some(e) => e,
             None => break 'main
         }, coords.1) {
-            if piece == &0 {
+            if piece == 0 {
                 coords.0 = coords.0.checked_add_signed(direction)?;
             }else if piece == original_piece {
                 self.set(coords.0.checked_add_signed(direction)?, coords.1, piece*2);
@@ -149,18 +160,23 @@ impl<const N: usize> Board<N> {
             }
         }
     
-        self.set(coords.0, coords.1, *original_piece);
+        self.set(coords.0, coords.1, original_piece);
         if coords.0 != x { self.set(x, y, 0); };
         Some(())
+    }
+
+    /// Get all pieces as a one-dim vector (used in js-side)
+    pub fn get_pieces(&self) -> Vec<Brick> {
+        self.pieces.iter().flatten().map(|e| *e).collect()
     }
 }
 
 /* Debug impl */
-impl<const N: usize> Debug for Board<N> {
+impl Debug for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut end = String::new();
 
-        for row in self.pieces {
+        for row in &self.pieces {
             for piece in row {
                 end += &format!("{:^4}", piece.to_string());
             }
