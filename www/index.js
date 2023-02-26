2d62a4a634bb21e888813317d5b0a0e4569be809
Artur <artur.h.0427@gmail.com>
2023-02-27 00:18:23 +0100

    New animation changes

--- www/index.js
+++ www/index.js
@@ -15,11 +15,14 @@ const SIZE = 4;
 /* Functionality */
 const merge_in_direction = (direction) => {
     let steps = JSON.parse(board.merge_all(direction));
-    board.set_random();
-
-    animate(steps, () => {
-        draw_grid();
-    });
+    if (steps.length > 0) {
+        let [x, y, num] = board.set_random();
+        animate_popup(x, y, num);
+        
+        animate(steps, () => {
+            draw_grid();
+        });
+    }
 };
 
 /* Event listeners */
@@ -75,6 +78,7 @@ const draw_grid = () => {
 
         const brick_container = document.createElement("div");
         brick_container.classList.add("brick-container");
+        brick_container.id = `shell-${x}-${y}`;
         brick_container.appendChild(brick);
 
         GAME_CONTAINER.appendChild(brick_container);
@@ -84,7 +88,7 @@ const draw_grid = () => {
 /* Animate steps */
 const animate = (steps, callback) => {
     steps.forEach(step => {
-        let { from_x, from_y, to_x, to_y } = step;
+        let { from_x, from_y, to_x, to_y, is_merge } = step;
         let brick = document.getElementById(`${from_x}-${from_y}`);
 
         let x_add = to_x - from_x;
@@ -92,18 +96,47 @@ const animate = (steps, callback) => {
         let x_perc = x_add*100;
         let y_perc = y_add*100;
 
-        brick.animate([
-            { transform: `translate(0%, 0%)` },
-            { transform: `translate(calc(${x_perc}% + ${x_add}vmin), calc(${y_perc}% + ${y_add}vmin))` },
-        ], {
-            duration: 80,
-            easing: "ease-in-out"
-        })
+
+        if (is_merge) {
+            brick.style.zIndex = 0;
+            brick.animate([
+                { transform: `translate(0%, 0%) scale(1)` },
+                { transform: `translate(calc(${x_perc}% + ${x_add}vmin), calc(${y_perc}% + ${y_add}vmin)) scale(0.6)` },
+            ], {
+                duration: 130,
+                easing: "ease-in-out"
+            })
+
+        }else {
+
+            brick.animate([
+                { transform: `translate(0%, 0%) scale(1)` },
+                { transform: `translate(calc(${x_perc}% + ${x_add}vmin), calc(${y_perc}% + ${y_add}vmin)) scale(1.1)` },
+            ], {
+                duration: 130,
+                easing: "ease-in-out"
+            })
+        }
     });
 
     setTimeout(() => {
         callback();
-    }, 80);
+    }, 130);
+};
+
+/* Animate piece getting set randomly */
+const animate_popup = (x, y, num) => {
+    const shell = document.getElementById(`shell-${x}-${y}`);
+
+    const brick = document.createElement("div");
+    brick.classList.add("brick-intro");
+    
+    const number = document.createElement("p");
+    number.innerText = num;
+    brick.classList.add(`brick-${num}`);
+
+    brick.appendChild(number);
+    shell.appendChild(brick);
 };
 
 /* Init */
